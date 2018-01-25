9722b370f1b136665b65a561a772e78596ecd8c0
Baseln <basel.obeid9661@gmail.com>
2018-01-25 09:10:05 +0100

    fixed some spacing

--- Weather Station API/DatabaseSetup/setupData.php
+++ Weather Station API/DatabaseSetup/setupData.php
@@ -1,42 +1,18 @@
 
-<?php 
+<?php
 
 function setupData(){
-
     $string = file_get_contents("../weather_data_170227 kl1234.csv");
-
-    $string = mb_convert_encoding($string,"auto","UCS-2LE");
+    $string = mb_convert_encoding($string, "auto", "UCS-2LE");
     $string =  utf8_encode($string);
-    $data = explode("\n",$string);
-
-
-
+    $data = explode("\n", $string);
     foreach($data as $key => $post){
-
-
-        $data[$key] = explode("\t",$post);
-
-
+        $data[$key] = explode("\t", $post);
         foreach ($data[$key] as $k => $field){
-
             $data[$key][$k] = trim(strval($field));
-
         }
-
-
-
-
     }
-
-
     return $data;
-
-
 }
-
-
-
-
-
 
 ?>
